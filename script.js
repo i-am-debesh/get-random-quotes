bf5dba93cf88aad89299c1bb39d4dc8ac026c6fd
Debesh Halder <debesh391@gmail.com>
2023-12-23 16:52:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const url = 'https://api.quotable.io/quotes/random';
+const quotetText = document.querySelector('.quote-text');
+const authorText = document.querySelector('.author');
+const getQuoteBtn = document.querySelector('.get-quote-btn');
+
+const clearField = ()=> {
+    quotetText.innerText = 'Fact :'
+    authorText.innerText = 'Author :';
+}
+const getQuote = async ()=>{
+    let response = await fetch(url);
+    let data = await response.json();
+    let quote = data[0].content;
+    let author = data[0].author;
+
+    quotetText.innerText = (quotetText.innerText)+` ${quote}`;
+    authorText.innerText = (authorText.innerText)+` ${author}`;
+}
+
+getQuoteBtn.addEventListener('click', ()=>{
+    clearField();
+    getQuote();
+});
