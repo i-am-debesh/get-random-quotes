85e84cc457413e652dfaf33975cc9dbf9600f8d7
Debesh <debesh391@gmail.com>
2024-03-23 18:42:01 +0530

    functioning with 50 quote

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-const url = '';
 
-// const quotetText = document.querySelector('.quote-text');
-// const authorText = document.querySelector('.author');
-// const getQuoteBtn = document.querySelector('.get-quote-btn');
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// const clearField = ()=> {
-//     quotetText.innerText = 'Quote :'
-//     authorText.innerText = 'Author :';
-// }
-// const getQuote = async ()=>{
-//     let response = await fetch(url);
-//     let data = await response.json();
-//     let quote = data[0].content;
-//     let author = data[0].author;
 
-//     quotetText.innerText = (quotetText.innerText)+` ${quote}`;
-//     authorText.innerText = (authorText.innerText)+` ${author}`;
-// }
+const quotetText = document.querySelector('.quote-text');
+const authorText = document.querySelector('.author');
+const getQuoteBtn = document.querySelector('.get-quote-btn');
+function setQuote(quote= 'hi folks') {
+    quotetText.innerText = "Quote : "+quote;
+}
+function setAuthor(author) {
+    authorText.innerText = "Author : "+author;
+}
 
-// getQuoteBtn.addEventListener('click', ()=>{
-//     clearField();
-//     getQuote();
-// });
+function getQuote() {
+    
 
+    fetch('quotes.json')
+    .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(jsonData => {
+    //console.log('Parsed JSON data:', jsonData);
+    const idx = getRandomInt(0,49);
+    const quote = jsonData[idx].q;
+    const author = jsonData[idx].a;
+
+    setQuote(quote);
+    setAuthor(author);
+    
+    
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  });
+
+
+}
+
+getQuote();
+
+getQuoteBtn.addEventListener('click', ()=>{
+    
+    // getQuote();
+    getQuote();
+});
 
